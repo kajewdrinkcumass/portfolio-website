0725f8fdd32e5f399ae6eced506ed92ac6720a56
kajewdrinkcumass <117846761+kajewdrinkcumass@users.noreply.github.com>
2023-01-10 12:46:00 +0100

    Added current condition

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ var j = 0,text4
 var g = 0,text5
 
 
-text2 = " cd /home/about_me"
+text2 = "cd /home/about_me"
 text3 = "  ls -a"
 text4 = " about_me.txt"
 text5 = "  sudo nano about_me.txt"
-const terminal = document.querySelector('.terminal');
+const terminal = document.querySelector('.terminal')
 
-
+let observer
 
 function typing2() {
   if (b < text2.length) {
@@ -24,18 +24,9 @@ function typing2() {
   } else {
     // function is done being executed
     document.getElementById("text3").innerHTML = "kajus@kajuswebsite:/home/about_me$"
-    typing3();
+    typing3()
   }
 }
-
-const observer = new IntersectionObserver((entries) => {
-  // If the element is visible on the screen, call the function
-  if (entries[0].isIntersecting) {
-    typing2();
-  }
-});
-
-observer.observe(terminal);
 
 function typing3() {
   if (k < text3.length) {
@@ -47,7 +38,7 @@ function typing3() {
       setTimeout(typing3, 100)
     }
   } else {
-    typing4();
+    typing4()
   }
 }
 
@@ -63,7 +54,7 @@ function typing4() {
   } else {
     // function is done being executed
     document.getElementById("text5").innerHTML = "kajus@kajuswebsite:/home/about_me$"
-    typing5();
+    typing5()
   }
 }
 
@@ -78,3 +69,51 @@ function typing5() {
     }
   }
 }
+
+function startTyping() {
+  typing2()
+  observer.disconnect()
+}
+
+observer = new IntersectionObserver((entries) => {
+  // If the element is visible on the screen, call the function
+  if (entries[0].isIntersecting) {
+    startTyping()
+  }
+})
+
+observer.observe(terminal)
+
+
+function replaceLetters() {
+  // Get the element with the class "test"
+  var element = document.querySelector('.name');
+
+  // Create an IntersectionObserver to watch for when the element is scrolled into view
+  var observer = new IntersectionObserver(function(entries) {
+    // If the element is scrolled into view
+    if (entries[0].isIntersecting) {
+      // Get the text inside the element
+      var text = element.textContent;
+
+      // Replace each letter in the text with a corresponding letter from the word "pizza"
+      // with a delay of 200 milliseconds between each letter
+      var i = 0;
+      function replace() {
+        element.textContent = element.textContent.slice(0, i) + "Hi, I'm Kajus Trinkunas"[i] + element.textContent.slice(i+1);
+        i++;
+        if (i < text.length) {
+          setTimeout(replace, 100);
+        }
+      }
+      replace();
+
+      // Disconnect the observer to prevent it from being triggered again
+      observer.disconnect();
+    }
+  });
+
+  // Start observing the element
+  observer.observe(element);
+}
+replaceLetters();
